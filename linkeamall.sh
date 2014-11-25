cba6ac1ff6fea441c302d9f1c5f909182571cf25
Julian Rath <julian.rath@gmail.com>
2014-11-25 15:23:28 +0000

    added autoamtic installation of neobundkle

--- linkeamall.sh
+++ linkeamall.sh
@@ -2,3 +2,6 @@ ln -s `pwd`/vim ~/.vim
 ln -s `pwd`/zshrc ~/.zshrc
 git config --global core.excludesfile ~/.dotfiles/gitignore
 
+mkdir -p ~/.vim/bundle
+git clone --depth 1 https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+
